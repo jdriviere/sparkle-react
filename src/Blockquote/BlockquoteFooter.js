e31f7b5ca0719b1d837945e4a4353df0290dd4a3
J. Djimitry Riviere <tilithar@gmail.com>
2018-09-06 23:14:22 -0600

    Adapted to new Sparkle framework feature.

---
+++ src/Blockquote/BlockquoteFooter.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+const Footer = ({
+    children,
+    className,
+    ...attributes
+}) => {
+    const classes = classNames('blockquote__footer', className);
+    return (
+        <footer className={classes} {...attributes}>
+            {children}
+        </footer>
+    );
+};
+
+Footer.propTypes = {
+    children: PropTypes.any,
+    className: PropTypes.string
+};
+
+export default Footer;
