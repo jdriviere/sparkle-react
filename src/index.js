0ded2202a11d6cb3cbb3e682cdb7a37cb1cf6fdf
J. Djimitry Riviere <tilithar@gmail.com>
2018-10-31 12:13:20 -0600

    Prep for future Components.

--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@
 // ========================================= //
 export { Alert } from './Alert';
 export { Blockquote } from './Blockquote';
+// export { Box } from './Box';
+// export { Boxes } from './Boxes';
 export { Button } from './Button';
 export { Buttons } from './Buttons';
+// export { Loader } from './Loader';
 export { Tag } from './Tag';
 export { Tags } from './Tags';
 
@@ -12,15 +15,17 @@ export { Tags } from './Tags';
 // FORMS
 // ========================================= //
 // export { Checkbox } from './Checkbox';
-// export { Radio } from './Radio';
 // export { Input } from './Input';
+// export { InputAddon } from './InputAddon';
 // export { Inputs } from './Inputs';
+// export { Radio } from './Radio';
 // export { Switch } from './Switch';
 
 // ========================================= //
 // LAYOUTS
 // ========================================= //
 export { Card } from './Card';
+// export { Container } from './Container';
 export { Grid } from './Grid';
 export { Hero } from './Hero';
 export { Media } from './Media';
